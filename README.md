03a48d4926ac748a25a0b53f17c9cbd7c9696299
slav123 <slav123@gmail.com>
2017-02-08 12:16:27 +0100

    init commit + readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
+Measures of Consumer Price Inflation
+
+From (Consumer Price Inflation)[http://www.rba.gov.au/inflation/measures-cpi.html]
--- scraper.php
+++ scraper.php
@@ -2,17 +2,70 @@
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
+$html = scraperwiki::scrape("http://www.rba.gov.au/inflation/measures-cpi.html");
 //
 // // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
+$dom = new simple_html_dom();
+$dom->load($html);
+$table = $dom->find("#year_ended tbody tr");
+
+$int = - 1;
+$fy  = array();
+
+foreach ($table as $row)
+{
+	$class = $row->class;
+	if ($class == 'tr-head')
+	{
+		$int   = 0;
+		$year  = $row->find('th', 0)->plaintext;
+		$fy    = explode('/', $year);
+		$fy[1] = '20' . $fy[1];
+
+	}
+	else
+	{
+		if ($int === - 1)
+		{
+			continue;
+		}
+
+		if ($month = $row->find('th', 0))
+		{
+			$month = $month->plaintext;
+		}
+
+		$date = $fy[$int] . '-' . date("m", strtotime($month));
+
+		// Consumer price index (All groups)
+		$cpi = $row->find('td', 0)->plaintext;
+
+		// Consumer price index (Excluding volatile items)
+		$cpi_evi = $row->find('td', 1)->plaintext;
+
+		//
+		if ($month === 'Dec')
+		{
+			$int ++;
+		}
+
+		if (is_numeric($cpi))
+		{
+			$data[] = array('date' => $date, 'cpi' => $cpi, 'cpi_evi' => $cpi_evi);
+		}
+
+	}
+}
+
+foreach ($data as $row)
+{
+	scraperwiki::save_sqlite(array('date'), $row);
+}
+
 // // Write out to the sqlite database using scraperwiki library
 // scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
 //
